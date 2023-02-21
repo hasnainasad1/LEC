1d65462c5d058c6c798c71f2826bc741bc6bd301
hasnainasad1 <hasnain.asad1999@gmail.com>
2023-02-22 04:03:10 +0500

    first commit

---
+++ javas.js
@@ -0,0 +1,10 @@
+$('.slide-carousel').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  variableWidth: true,
+  autoplay: true,
+  speed: 500,
+  autoplaySpeed: 4000
+});
